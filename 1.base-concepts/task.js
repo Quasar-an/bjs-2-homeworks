a5afe92dc1c7abeed824ec0b0e920b801615aaf9
Angelina <pankova1797@gmail.com>
2024-10-11 22:21:18 +0300

    Задания 1 и2 выполнены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,3 +19,29 @@ if (discriminant === 0) {
   roots.push(root);
   return roots;
 }
+
+console.log(solveEquation(1, -3, 2));
+console.log(solveEquation(1, 2, 1));
+console.log(solveEquation(1, 0, 1));
+
+
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let monthlyRate = (percent / 100) / 12;
+  let loanBody = amount - contribution;
+  if (loanBody <= 0) {
+    return 0;
+}
+
+let monthlyPayment = loanBody * (monthlyRate + (monthlyRate / (Math.pow(1 + monthlyRate, countMonths) - 1)));
+let totalAmount = monthlyPayment * countMonths;
+
+return Number(totalAmount.toFixed(2));
+}
+
+console.log(calculateTotalMortgage(10, 0, 50000, 12));
+console.log(calculateTotalMortgage(10, 1000, 50000, 12));
+console.log(calculateTotalMortgage(10, 0, 20000, 24));
+console.log(calculateTotalMortgage(10, 1000, 20000, 24));
+console.log(calculateTotalMortgage(10, 20000, 20000, 24));
+console.log(calculateTotalMortgage(10, 0, 10000, 36));
+console.log(calculateTotalMortgage(15, 0, 10000, 36));
